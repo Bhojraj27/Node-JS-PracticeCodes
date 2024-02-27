ff0d981a96ba69610708d66b10f13c82629dd9a2
bhojraj <bhojrajchavan01@gmail.com>
2024-02-27 12:34:41 +0530

    'mySQL DELETE api done

--- MYSQL-POST_API/index.js
+++ MYSQL-POST_API/index.js
@@ -21,15 +21,13 @@ app.post('/users', (req, res) => {
 app.put('/:id', (req, res) => {
     const { name, pass, user_type, city } = req.body;
     const id = req.params.id;
-    
+  
     // Check if all required fields are present
     if (!name || !pass || !user_type || !city) {
         return res.status(400).send("Missing required fields");
     }
-
     const sql = "UPDATE users SET name=?, pass=?, user_type=?, city=? WHERE id=?";
     const values = [name, pass, user_type, city, id];
-
     mysqlCon.query(sql, values, (err, result) => {
         if (err) {
             console.error("Error updating user:", err);
@@ -42,6 +40,23 @@ app.put('/:id', (req, res) => {
         res.status(200).send("User updated successfully");
     });
 });
+
+app.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "DELETE FROM users WHERE id=?";
+    const values = [id];
+    mysqlCon.query(sql, values, (err, result) => {
+        if (err) {
+            console.error("Error deleting user:", err);
+            return res.status(500).send("Error deleting user");
+        }
+        // Check if any rows were affected
+        if (result.affectedRows === 0) {
+            return res.status(404).send("User not found");
+        }
+        res.status(200).send("User deleted successfully");
+    })
+})
 
 app.listen(3000);
 
